105c8c1f97be6bdd3ba31a21d662da422ed006a5
Shunkicreate <69703740+Shunkicreate@users.noreply.github.com>
2021-10-10 15:55:08 +0900

    using

--- src/components/Minbootstrap.vue
+++ src/components/Minbootstrap.vue
@@ -9,7 +9,7 @@
                   Shunki Tada is a programer.
               </div>
               <div class="col-12">
-                  He can write basic HTML and CSS and he can create a website ueing Vue.js.
+                  He can write basic HTML and CSS and he can create a website using Vue.js.
               </div>
               <div class="col-12">
                   Moreover, he is familer to Python and C language. 
