757084bf37d96c73a6aea9faf0b52a3c2db489fc
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2021-12-08 22:56:08 +0900

    change icons

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,12 +2,14 @@
   <div class="about">
     <h1 class="title" style="font-size: 6ex">About me</h1>
     <div class="container">
-      <div class="row text-center justify-content-center history_row">
+      <div class="row text-center justify-content-center history_row gy-5">
         <div class="col-md-8 col-11 border border-secondary border-2 rounded-3">
           <div class="row">
             <div class="col-md-4">
               <div class="row justify-content-center content-title">
-                <div class="col-12"><h3>Games</h3></div>
+                <div class="col-12">
+                  <h3>Games</h3>
+                </div>
               </div>
               <div class="row justify-content-center">
                 <div class="col-lg-4 col-md-8 col-6">
@@ -38,13 +40,13 @@
             </div>
           </div>
         </div>
-      </div>
-      <div class="row text-center justify-content-center history_row">
         <div class="col-md-8 col-11 border border-secondary border-2 rounded-3">
           <div class="row">
             <div class="col-md-4">
               <div class="row justify-content-center content-title">
-                <div class="col-12"><h3>Music</h3></div>
+                <div class="col-12">
+                  <h3>Music</h3>
+                </div>
               </div>
               <div class="row justify-content-center">
                 <div class="col-lg-4 col-md-8 col-6">
@@ -53,15 +55,14 @@
                     width="auto"
                     height="auto"
                     fill="currentColor"
-                    class="bi bi-controller"
+                    class="bi bi-music-note-beamed"
                     viewBox="0 0 16 16"
                   >
                     <path
-                      d="M11.5 6.027a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm-1.5 1.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2.5-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm-1.5 1.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm-6.5-3h1v1h1v1h-1v1h-1v-1h-1v-1h1v-1z"
+                      d="M6 13c0 1.105-1.12 2-2.5 2S1 14.105 1 13c0-1.104 1.12-2 2.5-2s2.5.896 2.5 2zm9-2c0 1.105-1.12 2-2.5 2s-2.5-.895-2.5-2 1.12-2 2.5-2 2.5.895 2.5 2z"
                     />
-                    <path
-                      d="M3.051 3.26a.5.5 0 0 1 .354-.613l1.932-.518a.5.5 0 0 1 .62.39c.655-.079 1.35-.117 2.043-.117.72 0 1.443.041 2.12.126a.5.5 0 0 1 .622-.399l1.932.518a.5.5 0 0 1 .306.729c.14.09.266.19.373.297.408.408.78 1.05 1.095 1.772.32.733.599 1.591.805 2.466.206.875.34 1.78.364 2.606.024.816-.059 1.602-.328 2.21a1.42 1.42 0 0 1-1.445.83c-.636-.067-1.115-.394-1.513-.773-.245-.232-.496-.526-.739-.808-.126-.148-.25-.292-.368-.423-.728-.804-1.597-1.527-3.224-1.527-1.627 0-2.496.723-3.224 1.527-.119.131-.242.275-.368.423-.243.282-.494.575-.739.808-.398.38-.877.706-1.513.773a1.42 1.42 0 0 1-1.445-.83c-.27-.608-.352-1.395-.329-2.21.024-.826.16-1.73.365-2.606.206-.875.486-1.733.805-2.466.315-.722.687-1.364 1.094-1.772a2.34 2.34 0 0 1 .433-.335.504.504 0 0 1-.028-.079zm2.036.412c-.877.185-1.469.443-1.733.708-.276.276-.587.783-.885 1.465a13.748 13.748 0 0 0-.748 2.295 12.351 12.351 0 0 0-.339 2.406c-.022.755.062 1.368.243 1.776a.42.42 0 0 0 .426.24c.327-.034.61-.199.929-.502.212-.202.4-.423.615-.674.133-.156.276-.323.44-.504C4.861 9.969 5.978 9.027 8 9.027s3.139.942 3.965 1.855c.164.181.307.348.44.504.214.251.403.472.615.674.318.303.601.468.929.503a.42.42 0 0 0 .426-.241c.18-.408.265-1.02.243-1.776a12.354 12.354 0 0 0-.339-2.406 13.753 13.753 0 0 0-.748-2.295c-.298-.682-.61-1.19-.885-1.465-.264-.265-.856-.523-1.733-.708-.85-.179-1.877-.27-2.913-.27-1.036 0-2.063.091-2.913.27z"
-                    />
+                    <path fill-rule="evenodd" d="M14 11V2h1v9h-1zM6 3v10H5V3h1z" />
+                    <path d="M5 2.905a1 1 0 0 1 .9-.995l8-.8a1 1 0 0 1 1.1.995V3L5 4V2.905z" />
                   </svg>
                 </div>
               </div>
@@ -75,13 +76,13 @@
             </div>
           </div>
         </div>
-      </div>
-      <div class="row text-center justify-content-center history_row">
         <div class="col-md-8 col-11 border border-secondary border-2 rounded-3">
           <div class="row">
             <div class="col-md-4">
               <div class="row justify-content-center content-title">
-                <div class="col-12"><h3>Books</h3></div>
+                <div class="col-12">
+                  <h3>Books</h3>
+                </div>
               </div>
               <div class="row justify-content-center">
                 <div class="col-lg-4 col-md-8 col-6">
@@ -90,14 +91,11 @@
                     width="auto"
                     height="auto"
                     fill="currentColor"
-                    class="bi bi-controller"
+                    class="bi bi-book"
                     viewBox="0 0 16 16"
                   >
                     <path
-                      d="M11.5 6.027a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm-1.5 1.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2.5-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm-1.5 1.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm-6.5-3h1v1h1v1h-1v1h-1v-1h-1v-1h1v-1z"
-                    />
-                    <path
-                      d="M3.051 3.26a.5.5 0 0 1 .354-.613l1.932-.518a.5.5 0 0 1 .62.39c.655-.079 1.35-.117 2.043-.117.72 0 1.443.041 2.12.126a.5.5 0 0 1 .622-.399l1.932.518a.5.5 0 0 1 .306.729c.14.09.266.19.373.297.408.408.78 1.05 1.095 1.772.32.733.599 1.591.805 2.466.206.875.34 1.78.364 2.606.024.816-.059 1.602-.328 2.21a1.42 1.42 0 0 1-1.445.83c-.636-.067-1.115-.394-1.513-.773-.245-.232-.496-.526-.739-.808-.126-.148-.25-.292-.368-.423-.728-.804-1.597-1.527-3.224-1.527-1.627 0-2.496.723-3.224 1.527-.119.131-.242.275-.368.423-.243.282-.494.575-.739.808-.398.38-.877.706-1.513.773a1.42 1.42 0 0 1-1.445-.83c-.27-.608-.352-1.395-.329-2.21.024-.826.16-1.73.365-2.606.206-.875.486-1.733.805-2.466.315-.722.687-1.364 1.094-1.772a2.34 2.34 0 0 1 .433-.335.504.504 0 0 1-.028-.079zm2.036.412c-.877.185-1.469.443-1.733.708-.276.276-.587.783-.885 1.465a13.748 13.748 0 0 0-.748 2.295 12.351 12.351 0 0 0-.339 2.406c-.022.755.062 1.368.243 1.776a.42.42 0 0 0 .426.24c.327-.034.61-.199.929-.502.212-.202.4-.423.615-.674.133-.156.276-.323.44-.504C4.861 9.969 5.978 9.027 8 9.027s3.139.942 3.965 1.855c.164.181.307.348.44.504.214.251.403.472.615.674.318.303.601.468.929.503a.42.42 0 0 0 .426-.241c.18-.408.265-1.02.243-1.776a12.354 12.354 0 0 0-.339-2.406 13.753 13.753 0 0 0-.748-2.295c-.298-.682-.61-1.19-.885-1.465-.264-.265-.856-.523-1.733-.708-.85-.179-1.877-.27-2.913-.27-1.036 0-2.063.091-2.913.27z"
+                      d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"
                     />
                   </svg>
                 </div>
@@ -105,9 +103,9 @@
             </div>
             <div class="col-md-8 border-start border-secondary border-1 text-start">
               <ul class="lists">
-                <li class="list-items">告白  湊かなえ</li>
-                <li class="list-items">砂漠  伊坂幸太郎</li>
-                <li class="list-items">蜜蜂と遠雷  奥田陸</li>
+                <li class="list-items">告白 湊かなえ</li>
+                <li class="list-items">砂漠 伊坂幸太郎</li>
+                <li class="list-items">蜜蜂と遠雷 奥田陸</li>
               </ul>
             </div>
           </div>
@@ -118,7 +116,7 @@
 </template>
 
 <style lang="scss">
-.about{
+.about {
   font-family: serif, system-ui;
 }
 .card-css {
@@ -127,33 +125,33 @@
 .card-css-bottom {
   margin-bottom: 3em;
 }
-.lists{
-  margin:4em 5em;
+.lists {
+  margin: 4em 5em;
 }
-.list-items{
+.list-items {
   line-height: 2em;
   font-size: 1.3em;
 }
-.content-title{
-  margin:3em auto;
+.content-title {
+  margin: 3em auto;
 }
-@media screen and (max-width:992px){
-  .lists{
+@media screen and (max-width: 992px) {
+  .lists {
     margin: 4em 2em;
   }
-//   .content-title{
-//   margin-top:1em;
-//   margin-bottom: 1em
-// }
+  //   .content-title{
+  //   margin-top:1em;
+  //   margin-bottom: 1em
+  // }
 }
-@media screen and (max-width:767px){
-  .lists{
+@media screen and (max-width: 767px) {
+  .lists {
     margin: 10% 15%;
   }
-//   .content-title{
-//   margin-top:1em;
-//   margin-bottom: 1em
-// }
+  //   .content-title{
+  //   margin-top:1em;
+  //   margin-bottom: 1em
+  // }
 }
 </style>
 
