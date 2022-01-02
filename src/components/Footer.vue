2dd887e940ba1681f67dd88632e415c40f992121
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2022-01-02 12:52:54 +0900

    please dont happen conflict

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,6 +6,7 @@
           <a
             href="https://www.instagram.com/shunki__t/"
             target="_blank"
+            rel="noopener noreferrer"
             class="sns"
           >
             <svg
@@ -24,7 +25,12 @@
         </div>
 
         <div class="col-md-1 col-4">
-          <a href="https://twitter.com/syunki____" target="_blank" class="sns">
+          <a
+            href="https://twitter.com/syunki____"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="sns"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               :width="icon_size"
@@ -44,6 +50,7 @@
           <a
             href="mailto:jmsrsyunrinsyunki@gmail.com"
             target="_blank"
+            rel="noopener noreferrer"
             class="sns"
           >
             <svg
@@ -65,6 +72,7 @@
           <a
             href="https://www.facebook.com/profile.php?id=100010753628988"
             target="_blank"
+            rel="noopener noreferrer"
             class="sns"
           >
             <svg
@@ -86,6 +94,7 @@
           <a
             href="https://www.linkedin.com/in/%E9%A7%BF%E5%B8%8C-%E5%A4%9A%E7%94%B0-1450891a7/"
             target="_blank"
+            rel="noopener noreferrer"
             class="sns"
           >
             <svg
@@ -107,22 +116,23 @@
           <a
             href="https://github.com/Shunkicreate/"
             target="_blank"
+            rel="noopener noreferrer"
             class="sns"
           >
-          <div class="under-button">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              :width="icon_size"
-              :height="icon_size"
-              fill="currentColor"
-              class="bi bi-github"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-              />
-            </svg>
-          </div>
+            <div class="under-button">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                :width="icon_size"
+                :height="icon_size"
+                fill="currentColor"
+                class="bi bi-github"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                />
+              </svg>
+            </div>
           </a>
         </div>
       </div>
@@ -144,22 +154,18 @@ export default defineComponent({
 </script>
 
 <style>
-
-
 .footer {
   padding: 2em;
-  
 }
 
-a:hover path{
+a:hover path {
   color: #c9c8c8;
   transition: 0.5s;
 }
-a:visited{
-  color: #2c3e50; 
-}
-a:link{
+a:visited {
   color: #2c3e50;
 }
-
+a:link {
+  color: #2c3e50;
+}
 </style>
--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -7,7 +7,9 @@
         </div>
         <div class="col-md-6 col-12">
           <div class="row justify-content-center row-content text-start gy-3">
-            <div class="col-12"><h4>My first work</h4></div>
+            <div class="col-12">
+              <h4>My first work</h4>
+            </div>
             <div>
               This is my first work in programming. I love Mario so much that I
               was obsessed with it when I was a first year in college. The web
@@ -17,55 +19,86 @@
             <div class="col-md-12 text-start">
               <div class="row justify-content-evenly text-start">
                 <div class="col-md-2 col-auto text">
-                  <a target="__blank" href="https://t.co/dNOr2MoAXa?amp=1"
-                    ><button type="button" class="btn btn-outline-secondary">
-                      website
-                    </button></a
-                  >
+                  <a target="_blank" rel="noopener noreferrer" href="https://t.co/dNOr2MoAXa?amp=1">
+                    <button type="button" class="btn btn-outline-secondary">website</button>
+                  </a>
                 </div>
                 <div class="col-md-2 col-auto text">
                   <a
-                    target="__blank"
+                    target="_blank"
+                    rel="noopener noreferrer"
                     href="https://twitter.com/syunki____/status/1269528018076102657?s=20"
-                    ><button type="button" class="btn btn-outline-secondary">
-                      Video
-                    </button></a
                   >
+                    <button type="button" class="btn btn-outline-secondary">Video</button>
+                  </a>
                 </div>
               </div>
             </div>
-            <div class="col-12"><h4>My Works</h4></div>
+            <div class="col-12">
+              <h4>My Works</h4>
+            </div>
             <div class="col-md-12">
               <ul>
-                <li>美女図鑑<a href="https://shunkicreate.github.io/beautiful_girl/" target="__blank"><button type="button" class="btn btn-outline-secondary">https://shunkicreate.github.io/beautiful_girl/</button></a></li>
+                <li>
+                  美女図鑑
+                  <a
+                    href="https://shunkicreate.github.io/beautiful_girl/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <button
+                      type="button"
+                      class="btn btn-outline-secondary"
+                    >https://shunkicreate.github.io/beautiful_girl/</button>
+                  </a>
+                </li>
                 <li>晩飯</li>
               </ul>
             </div>
-            <div class="col-12"><h4>Internships</h4></div>
+            <div class="col-12">
+              <h4>Internships</h4>
+            </div>
             <div class="col-md-12">
               <ul>
-                <li>Potlatch | Jan-Feb 2021</li>
-                <li>Hutzper | Aug- 2021</li>
-                <li>Simplise | Aug- 2021</li>
+                <li>
+                  <a
+                    href="https://www.trypotlatch.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >Potlatch</a> | Jan-Feb 2021
+                </li>
+                <li>
+                  <a
+                    href="https://hutzper.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >Hutzper</a> | Aug- 2021
+                </li>
+                <li>
+                  <a
+                    href="https://simplise.jp/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >Simplise</a> | Aug- 2021
+                </li>
               </ul>
             </div>
-            <div class="col-12"><h4>Who is he? </h4></div>
+            <div class="col-12">
+              <h4>Who is he?</h4>
+            </div>
             <div class="col-12">Shunki Tada is a programmer.</div>
             <div class="col-12">
               He can write basic HTML and CSS and he can create a website using
               Vue.js.
             </div>
-            <div class="col-12">
-              Moreover, he is familior with Python and C language.
-            </div>
+            <div class="col-12">Moreover, he is familior with Python and C language.</div>
             <div class="col-12">
               <router-link
                 to="/history"
                 target="_top"
                 class="nav-link active"
                 aria-current="page"
-                >Learn more</router-link
-              >
+              >Learn more</router-link>
             </div>
           </div>
         </div>
